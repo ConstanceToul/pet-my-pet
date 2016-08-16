dffe34d650b07024f44ced0f28a9240f0c17f7b6
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-08-16 18:19:16 +0200

    working on pets#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Pet.create(name: "Constance", title: "What a nice picture", species: "dog", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly dog with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Andrea", title: "What a nice picture", species: "cat", gender: "male", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly cat with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Aoife", title: "What a nice picture", species: "horse", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly horse with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Michael", title: "What a nice picture", species: "camel", gender: "male", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly camel with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Britney", title: "What a nice picture", species: "hamster", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly hamster with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Christina", title: "What a nice picture", species: "lemur", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly lemur with lots of energy; unfortunately has a peeing problem", price: 20)
