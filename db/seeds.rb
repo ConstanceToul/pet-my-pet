7967a841ddd75da9241af1ce26a1dfa26678aa90
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-08-16 18:49:43 +0200

    first day work on pets#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 
 
-Pet.create(name: "Constance", title: "What a nice picture", species: "dog", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly dog with lots of energy; unfortunately has a peeing problem", price: 20)
-Pet.create(name: "Andrea", title: "What a nice picture", species: "cat", gender: "male", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly cat with lots of energy; unfortunately has a peeing problem", price: 20)
-Pet.create(name: "Aoife", title: "What a nice picture", species: "horse", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly horse with lots of energy; unfortunately has a peeing problem", price: 20)
-Pet.create(name: "Michael", title: "What a nice picture", species: "camel", gender: "male", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly camel with lots of energy; unfortunately has a peeing problem", price: 20)
-Pet.create(name: "Britney", title: "What a nice picture", species: "hamster", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly hamster with lots of energy; unfortunately has a peeing problem", price: 20)
-Pet.create(name: "Christina", title: "What a nice picture", species: "lemur", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly lemur with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Constance", title: "Beautiful beast", species: "dog", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly dog with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Andrea", title: "Timid tiger", species: "cat", gender: "male", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly cat with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Aoife", title: "Hung like a horse", species: "horse", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly horse with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Michael", title: "A camel for hump day", species: "camel", gender: "male", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly camel with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Britney", title: "Beautiful hammie", species: "hamster", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly hamster with lots of energy; unfortunately has a peeing problem", price: 20)
+Pet.create(name: "Christina", title: "Super friendly lemur", species: "lemur", gender: "female", picture_url: "https://unsplash.it/200/300?image=1084", description: "Friendly lemur with lots of energy; unfortunately has a peeing problem", price: 20)
