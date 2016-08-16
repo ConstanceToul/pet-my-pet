72060d5ec602ccf147c265d45e978d3a74990f6a
Constance Toulemonde <toulemonde.const@gmail.com>
2016-08-16 18:15:59 +0200

    simple form and figaro install

--- app/controllers/owner/pets_controller.rb
+++ app/controllers/owner/pets_controller.rb
@@ -1,14 +1,30 @@
 class Owner::PetsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :new
+
   def index
-    # Let's anticipate on next week (with login)
     @pets = current_user.pets
   end
 
   def new
+    #@species = ["ostrich", "monkey", "dog", "cat", "goat", "rabbit", "hamster", "snake", "rat", "mouse", "chameleon", "swan", "elephant", "pelican", "bird", "tortoise", "gnu", "otter", "pig", "parrot", "lion"]
+    @pet = Pet.new
   end
 
   def create
-    @pet.save
-    redirect_to
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to owner_pet_path(@pet)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :title, :species, :gender, :birth_date,
+     :height, :weight, :picture_url, :description, :food, :exercise, :price)
+  end
+
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,11 @@
 class Pet < ApplicationRecord
+  SPECIES = ["dog", "cat", "ostrich", "monkey", "goat", "rabbit", "hamster", "snake", "rat", "mouse", "chameleon", "swan", "elephant", "pelican", "bird", "tortoise", "gnu", "otter", "pig", "parrot", "lion"].sort
 
   has_many :bookings
   belongs_to :user
 
   validates :name, presence: true
-  validates :species, presence: true, , inclusion: { in: ["ostrich", "monkey", "dog", "cat", "goat", "rabbit", "hamster", "snake", "rat", "mouse", "chameleon", "swan", "elephant", "pelican", "bird", "tortoise", "gnu", "otter", "pig", "parrot", "lion"], allow_nil: false }
+  validates :species, presence: true, inclusion: { in: SPECIES, allow_nil: false }
   validates :title, presence: true
   validates :gender, presence: true, inclusion: { in: ["Female", "Male"], allow_nil: false }
   validates :price, presence: true
