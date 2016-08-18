1c48c566fce13a5fcfc77fa473fd83949d1143ab
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-08-18 13:47:00 +0200

    a few adjustments to button and start renter model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,13 @@
 class BookingsController < ApplicationController
   def create
     @pet = Pet.find(params[:pet_id])
-    @booking = Booking.new(pet_id: @pet, user_id: current_user)
+    @booking = Booking.new
+    @booking.pet = @pet
+    @booking.user = current_user
+    @booking.price = @pet.price
+
+    @booking.save
+    raise
   end
 
 end
